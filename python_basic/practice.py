e4350eb23b13e36c4a5ae77e4587357c52ac5af6
Gnadi4 <uno6070w@gmail.com>
2021-03-29 11:22:44 +0900

    20210329 morning

--- python_basic/practice.py
+++ python_basic/practice.py
@@ -862,3 +862,83 @@
 # valkyrie = FlyableAttackUnit("valkyrie", 200, 6, 5)
 # valkyrie.fly(valkyrie.name, "3시")
 
+### 연산자 오버로딩 ###
+
+class Unit:
+    def __init__(self, name, hp, speed):
+        self.name = name
+        #self.damage = damage
+        self.hp = hp
+        self.speed = speed
+        #print("{0} unit is making".format(self.name))
+        #print("hp {0}, attack {1}".format(self.hp, self.damage))
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        #self.name = name
+        #self.hp = hp
+        Unit.__init__(self, name, hp, speed)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+            .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name, self.hp))
+        if self.hp<=0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) ##지상 스피드 0
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+# vulture = AttackUnit("벌쳐", 80, 10, 20)
+
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+# vulture.move("11시")
+# #battlecruiser.fly("배틀크루저","9시")
+# battlecruiser.move("9시")
+
+### pass ###
+
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         pass
+
+# supply_depot = BuildingUnit("서플라이", 500, "7시")
+
+# def game_start():
+#     print("[알림] 새로운 게임을 시작합니다.")
+
+# def game_over():
+#     pass
+
+# game_start()
+# game_over()
+
+### super ###
+
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        #Unit.__init__(self,name, hp, 0)
+        super().__init__(name,hp,0)
+        self.location = location
